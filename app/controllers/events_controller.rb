a100422b811f9b8f55f523e395e0918c6b96adbe
Shree Dittakavi <shreekavi@gmail.com>
2010-09-25 16:04:28 +0530

    Updated Calendar with Event status and subjects

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,50 +1,156 @@
+require 'nokogiri'
+
 class EventsController < ApplicationController
   
   layout 'application', :except=>['signage']
   def index
-  	@events = Event.find(:all)
+
+	
+
   end
 
   def signage
-    	@events = Event.find(:all)
-  end
+		#Read the request for a meeting room from URL
+	
+		@room_name = params[:id].upcase
 
-  def current_meeting
-  ## A general purpose action to get the details of Current Event
-  ## Get the current time and Date.
-  ## Find a record that matches current Date and time
-  ## Mark it as meeting started
-  ## Render the current meeting on View
-	@events = Event.find(:all, :conditions=>["event_date = :eventdate",{:eventdate => Date.today}])
-  	render :partial => 'current_meeting'
-  end
+	  	@shift1=["9:00","9:30","10:00","10:30","11:00","11:30","12:00","12:30","13:00","13:30","14:00","14:30","15:00","15:30","16:00","16:30","17:00","17:30","18:00","18:30"]
+
+		@shift2=["17:30","18:00","18:30","19:00","19:30","20:00","20:30","21:00","21:30","22:00","22:30","23:00","23:30","00:00","00:30","1:00","1:30","2:00","2:30"]
+
+		@shift3=["1:30","2:00","2:30","3:00","3:30","4:00","4:30","5:00","5:30","6:00","6:30","7:00","7:30","8:00","8:30","9:00","9:30","10:00","10:30"]
+
+		#Get the Current Shift based on server timestamp
+	  	@current_shift = Event.current_shift
+	  	
+	  	#Get the Shift Time
+	  	@current_time = Time.now.strftime("%H:%M")
+	
+		#mail = params[:id] + '@shellnetworks.com'
+		mail = "shree@shellnetworks.com"
+		today = Time.now.strftime("%Y-%m-%d")
+		#today = "2010-09-28"
+		start_time = today +"T00:00:00"
+		end_time = today + "T23:59:59"
+
+		#This XML document can go into a private method
+	
+		events = search_events(mail, start_time, end_time)
+
+		#################
+		@start_times = []
+		@end_times = []
+		@subjects = []
+		events.each do |k|
+			@start_times << k["StartTime"]
+			@end_times << k["EndTime"]
+			@subjects << k["Subject"]
+		end 
+	end
+	
+		
+
   
-  def past_meetings
-  ## A general purpose action to get all the past meetings sorted by Date ( Show top 5)
-  ## Get the current time and Date.
-  ## Find all the records that are marked as completed / closed
-  ## Sort the records in descending order or Date and time
-  ## Pick Top 5 and render them on the view
-  	@events = Event.past_meetings
- 	render :partial => 'past_meetings'
-  end
+  private
   
-  def future_meetings
-  ## A general purpose action to get all the future meeting sorted by Date and time ( Show top 5)
-  ## Get current Date and time
-  ## Find all the records that are not marked as completed
-  ## Sort the records in ascending order of Date and Time
-  	@events = Event.future_meetings
-  	render :partial => 'future_meetings'
-  end
+  def search_events(mailbox, starttime, endtime)
   
-  def rest_of_today_meetings
-  ## A general purpose action to get rest of the meetings in the day looking at the current time stamp
-  ## Get Current Date and time
-  ## Find all the records that belong to current day
-  ## Pick all the records and render them in view in descending order
-      	render :partial => 'rest_of_today'
+  	
+doc = Nokogiri::XML <<-EOXML1
+<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+               xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+               xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"
+               xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types">
+  <soap:Body>
+    <GetUserAvailabilityRequest xmlns="http://schemas.microsoft.com/exchange/services/2006/messages"
+                xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types">
+      <t:TimeZone xmlns="http://schemas.microsoft.com/exchange/services/2006/types">
+        <Bias>-330</Bias>
+        <StandardTime>
+          <Bias>0</Bias>
+          <Time>00:00:00</Time>
+          <DayOrder>5</DayOrder>
+          <Month>10</Month>
+          <DayOfWeek>Sunday</DayOfWeek>
+        </StandardTime>
+        <DaylightTime>
+          <Bias>0</Bias>
+          <Time>00:00:00</Time>
+          <DayOrder>1</DayOrder>
+          <Month>4</Month>
+          <DayOfWeek>Sunday</DayOfWeek>
+        </DaylightTime>
+      </t:TimeZone>
+      <MailboxDataArray>
+        <t:MailboxData>
+          <t:Email>
+            <t:Address>#{mailbox}</t:Address>
+          </t:Email>
+          <t:AttendeeType>Required</t:AttendeeType>
+          <t:ExcludeConflicts>true</t:ExcludeConflicts>
+        </t:MailboxData>
+      </MailboxDataArray>
+      <t:FreeBusyViewOptions>
+        <t:TimeWindow>
+          <t:StartTime>#{starttime}</t:StartTime>
+          <t:EndTime>#{endtime}</t:EndTime>
+        </t:TimeWindow>
+        <t:MergedFreeBusyIntervalInMinutes>30</t:MergedFreeBusyIntervalInMinutes>
+        <t:RequestedView>Detailed</t:RequestedView>
+      </t:FreeBusyViewOptions>
+    </GetUserAvailabilityRequest>
+  </soap:Body>
+</soap:Envelope>
+
+EOXML1
+
+		puts doc
+		wsdl = `curl -v -u shreeram:Shell123 -L "https://shell.shellnetworks.com/ews/exchange.asmx" -d '#{doc.to_xml}' -H "Content-Type:text/xml"`
+		response_xml = Nokogiri::XML(wsdl)
+		puts response_xml
+
+
+		aoh = []
+		response_xml.remove_namespaces!
+
+		 response_xml.xpath("//CalendarEventArray").children.each do |d|
+		 	puts "Child"
+	  		h1 = Hash.new
+	   		d.children.each do |f|
+				if f.name =="StartTime"
+					h1["StartDay"] = f.text[0..9]
+					if f.text[11] == 48 # ASCII for 0
+						h1["StartTime"] = f.text[12..15]
+					else
+						h1["StartTime"] = f.text[11..15]
+					end
+				elsif f.name =="EndTime"
+					h1["EndDay"] = f.text[0..9]
+					if f.text[11] == 48 # ASCII for 0
+						h1["EndTime"] = f.text[12..15]
+					else
+						h1["EndTime"] = f.text[11..15]
+					end
+				else
+	  				h1[f.name] = f.text
+				end	  	
+				
+				##This block is for getting calendar Subject Details
+				if f.name=="CalendarEventDetails"
+				   f.children.each do |ce|
+				   	if ce.name=="Subject"
+				   		h1["Subject"] = ce.text
+				   	end
+				   end
+				end
+				##
+
+	  		end
+	  	
+	  		aoh << h1
+	  end
+	return aoh
   end
-  
 
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,63 @@
 class Event < ActiveRecord::Base
-	  named_scope :today, 
-	  	      :conditions => {:event_date=>"#{Date.today.strftime("%Y-%m-%d")}"}
-	  #named_scope :current_meeting
-	  named_scope :past_meetings,
-	  	      :order => "event_date ASC", 
-	  	      :conditions => ["event_date < :eventdate",{:eventdate => Date.today}]
-	  named_scope :future_meetings,
-	              :order => "event_date DESC", 
-	              :conditions => ["event_date > :eventdate",{:eventdate => Date.today}]
+
+	def self.current_year
+		Time.now.strftime("%Y").to_i
+	end
+	
+	def self.current_month
+		Time.now.strftime("%m").to_i	
+	end
+	
+	def self.current_day
+		Time.now.strftime("%d").to_i	
+	end
+	
+	def self.current_hour
+		Time.now.strftime("%H").to_i	
+	end
+	
+	def self.current_minutes
+		Time.now.strftime("%M").to_i	
+	end
+	def self.shift1_start_time
+		Time.local(current_year, current_month, current_day, 9, 30, 00)
+	end
+	
+	def self.shift1_end_time
+		Time.local(current_year, current_month, current_day, 18, 30, 00)
+	end
+	
+	def self.shift2_start_time
+		Time.local(current_year, current_month, current_day, 17, 30, 00)
+	end
+	
+	def self.shift2_end_time
+		Time.local(current_year, current_month, current_day+1, 2, 30, 00)
+	end	
+
+	def self.shift3_start_time
+		Time.local(current_year, current_month, current_day, 1, 30, 00)
+	end
+	
+	def self.shift3_end_time
+		Time.local(current_year, current_month, current_day, 10, 30, 00)
+	end
+
+	def self.current_shift
+		# This can be current time, refactor it later
+		#event_time_stamp = Time.local(2010, 9, 24, 2, 29,00)
+		event_time_stamp = Time.now
+		if event_time_stamp.between?(shift1_start_time, shift1_end_time)
+			return "shift1"
+		end
+
+		if event_time_stamp.between?(shift2_start_time, shift2_end_time)
+			return "shift2"
+		end
+
+		if event_time_stamp.between?(shift3_start_time, shift3_end_time)
+			return "shift3"
+		end
+
+	end
 end
